561d31bdab3069a9ff51fc0c3c1fb02a0d6998df
Diego Vissini <diego@vissini.com.br>
2023-02-10 19:14:56 -0300

    test new

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
 {
     /*
     |--------------------------------------------------------------------------
-    | Login Controller
+    | Login Controller test
     |--------------------------------------------------------------------------
     |
     | This controller handles authenticating users for the application and
