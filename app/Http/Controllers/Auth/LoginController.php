2c2ad6f8ef927b4a4a8de1f9aab02fc49070e6b3
Diego Vissini <diego@vissini.com.br>
2023-02-10 19:06:54 -0300

    Revert "test revert"
    This reverts commit f0a65a61d9d2399f176d3ba4d394be84d462483d.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
 {
     /*
     |--------------------------------------------------------------------------
-    | Login Controller test
+    | Login Controller
     |--------------------------------------------------------------------------
     |
     | This controller handles authenticating users for the application and
